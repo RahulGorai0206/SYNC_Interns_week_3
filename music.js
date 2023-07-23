2f224198e4dd9e1947345d909f339e9d1db82c42
RahulGorai0202 <rahulgorai0206@gmail.com>
2023-07-23 09:24:07 +0530

    Set details on song change & onload done

--- music.js
+++ music.js
@@ -8,7 +8,6 @@ const artist=document.querySelector('#artist');
 const title=document.querySelector('#title');
 const ind=document.querySelector('#ind');
 
-let indexPointer=0;
 let prevSongArray=new Array();
 const url = "https://raw.githubusercontent.com/RahulGorai0206/songs/main/song.json";
 
@@ -28,23 +27,35 @@ function pauseMusic(){
     pause.style.display="none";
     play.style.display="inline-flex";
 }
+function setDetails(songObject,songIndex){
+    songImage.src="https://raw.githubusercontent.com/RahulGorai0206/songs/main/images/"+songObject[songIndex].image;
+    title.textContent=`${songObject[songIndex].title}`;
+    artist.textContent=`${songObject[songIndex].artist}`;
+}
+function autoFetch(){
+    songImage.src="https://raw.githubusercontent.com/RahulGorai0206/songs/main/images/1.jpg";
+    title.textContent=`APNA BANA LE PIYA`;
+    artist.textContent=`Arijit Singh`;
+    music.src="https://raw.githubusercontent.com/RahulGorai0206/songs/main/music/1.mp3";
+}
 async function nextSong(){
     const objectOfObject = await fetchJSON();
     let x =Math.floor((Math.random() * objectOfObject.length) + 1);
-    music.src="https://raw.githubusercontent.com/RahulGorai0206/songs/main/music/"+x+".mp3"
+    music.src="https://raw.githubusercontent.com/RahulGorai0206/songs/main/music/"+x+".mp3";
     prevSongArray.push(x);
     playMusic();
-    console.log(prevSongArray)
+    setDetails(objectOfObject,x-1);
 }
 async function prevSong(){
     const objectOfObject = await fetchJSON();
     if(prevSongArray.length>1){
-        music.src="https://raw.githubusercontent.com/RahulGorai0206/songs/main/music/"+prevSongArray[prevSongArray.length-2]+".mp3"
+        music.src="https://raw.githubusercontent.com/RahulGorai0206/songs/main/music/"+prevSongArray[prevSongArray.length-2]+".mp3";
         prevSongArray.pop(prevSongArray.length)
     }else{
-        music.src="https://raw.githubusercontent.com/RahulGorai0206/songs/main/music/1.mp3"
+        music.src="https://raw.githubusercontent.com/RahulGorai0206/songs/main/music/1.mp3";
     }
     playMusic();
+    setDetails(objectOfObject);
 }
 play.addEventListener("click",()=>{
     playMusic();
